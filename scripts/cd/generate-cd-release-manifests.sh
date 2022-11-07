d19f5f3d4ea54243b5734501528eb38b9109ac3d
Francisc Munteanu <fmuntean@redhat.com>
2022-11-07 09:56:18 +0100

    add `--image-platform` parameter to support cross-platform docker image builds. Fix olm-setup.sh path when sourced from locally.

--- scripts/cd/generate-cd-release-manifests.sh
+++ scripts/cd/generate-cd-release-manifests.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
 
+# get abs path of the directory where this script is located
+# this way the source of the local file can work independently of where the script is executed from.
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 # use the olm-setup as the source
-OLM_SETUP_FILE=scripts/cd/olm-setup.sh
+OLM_SETUP_FILE=${SCRIPT_DIR}/olm-setup.sh
 OWNER_AND_BRANCH_LOCATION=${OWNER_AND_BRANCH_LOCATION:-codeready-toolchain/toolchain-cicd/master}
 
 if [[ -f ${OLM_SETUP_FILE} ]]; then
