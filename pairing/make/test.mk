2e5ff2fcc855a403c4ca9aac6d006b3fa03d18d2
Rafaela Soares <rsoaresd@redhat.com>
2025-06-09 10:11:17 +0100

    requested changes

--- pairing/make/test.mk
+++ pairing/make/test.mk
@@ -2,3 +2,16 @@
 test: build
 	@echo "running the pairing unit tests..."
 	go test -v -failfast ./...
+
+
+# Output directory for coverage information
+COV_DIR = $(OUT_DIR)/coverage
+
+.PHONY: test-with-coverage
+## runs the tests with coverage
+test-with-coverage:
+	@echo "running the unit tests with coverage..."
+	@-mkdir -p $(COV_DIR)
+	@-rm $(COV_DIR)/coverage.txt
+	$(Q)go test -vet off ${V_FLAG} $(shell go list ./... | grep -v /cmd/manager) -coverprofile=$(COV_DIR)/coverage.txt -covermode=atomic ./...
+
