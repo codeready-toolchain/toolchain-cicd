40c35769ca3a99fa1aad9e9d0619a11b46da3a0a
fmuntean <fmuntean@redhat.com>
2026-03-11 11:16:39 +0100

    detect go version from go mod

--- govulncheck-action/entrypoint.sh
+++ govulncheck-action/entrypoint.sh
@@ -1,11 +1,22 @@
 #!/bin/bash
 
-# upgrade the go version to match the `toolchain` directive in the `go.mod` file
-# see https://go.dev/doc/toolchain
-export GOTOOLCHAIN=auto
+# Read the toolchain directive from the scanned project's go.mod so that govulncheck
+# analyses the code against the Go stdlib version the project actually uses, not the
+# Go version baked into this container.
+# See https://go.dev/doc/toolchain
+GOMOD_TOOLCHAIN=$(grep '^toolchain ' go.mod 2>/dev/null | awk '{print $2}')
+GOMOD_GO=$(grep '^go ' go.mod 2>/dev/null | awk '{print $2}')
+if [ -n "$GOMOD_TOOLCHAIN" ]; then
+    export GOTOOLCHAIN="$GOMOD_TOOLCHAIN"
+elif [ -n "$GOMOD_GO" ]; then
+    export GOTOOLCHAIN="go${GOMOD_GO}"
+else
+    export GOTOOLCHAIN=auto
+fi
+
 go mod verify
 
-# Check the version of Go
+# Check the version of Go that will be used for scanning
 go version
 
 # Run the govulncheck command
